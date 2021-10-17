533d213178b3347bff9e839bcd948e2c8c676181
Marcomattia Mocellin <blondemambasoftware@gmail.com>
2021-10-17 22:20:27 +0200

    refactoring

--- Interlogica.Pastry.BackEnd/Startup.cs
+++ Interlogica.Pastry.BackEnd/Startup.cs
@@ -49,8 +49,11 @@ namespace Interlogica.Pastry.BackEnd
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AppDbContext appDbContext)
         {
+
+            appDbContext.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -76,6 +79,8 @@ namespace Interlogica.Pastry.BackEnd
                 endpoints.MapControllers();
 
             });
+
+
         }
     }
 }
